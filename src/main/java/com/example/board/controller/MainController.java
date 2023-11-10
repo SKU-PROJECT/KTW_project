818ecbfbc5080b0705db1b477ca91676d1e3e97d
eunjeong Kim <guri2913@gmail.com>
2023-11-10 14:47:30 +0900

    maincontroller 수정필요

--- src/main/java/com/example/board/controller/MainController.java
+++ src/main/java/com/example/board/controller/MainController.java
@@ -10,4 +10,10 @@ public class MainController {
         // 여행지 소개를 클릭하면 메인 페이지로 이동하는 뷰 이름을 반환합니다.
         return "mainPage";
     }
+
+    @GetMapping("/stay")
+    public String stayPage() {
+
+        return "stayPage";
+    }
 }
