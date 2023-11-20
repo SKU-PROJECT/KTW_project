180343dc0ef93a5091223b2af59166d77fafa74b
eunjeong Kim <guri2913@gmail.com>
2023-11-20 11:03:55 +0900

    Member_spring security 설정

---
+++ src/main/java/com/example/Security/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.Security.repository;
+
+import com.example.Security.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+
+}
