285f3a3a480055da9d2097f3c2339ca098f6eeb8
eunjeong Kim <guri2913@gmail.com>
2023-11-22 12:08:40 +0900

    Board, eatery 삭제함으로써 충돌해결

--- src/main/java/com/example/Board/Repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Board.Repository;
-
-import com.example.Board.Entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
--- src/main/java/com/example/Eatery/Repository/EateryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Eatery.Repository;
-
-import com.example.Eatery.Entity.Eatery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EateryRepository extends JpaRepository<Eatery, Long> {
-
-}
