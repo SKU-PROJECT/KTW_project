9c477b1852fd45a397bb6cf985c75b52bd188bc1
eunjeong Kim <guri2913@gmail.com>
2023-11-29 22:29:13 +0900

    관심리스트 수정

---
+++ src/main/java/com/example/Cart/Repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.example.Cart.Repository;
+
+import com.example.Cart.Entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
